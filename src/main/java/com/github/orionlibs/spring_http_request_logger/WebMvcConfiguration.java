0dec96e4ee7f4f99de03370d64722986c1df531e
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-05 17:10:08 +0100

    --created the interceptor --created the JavaConfig class that registers the interceptor with spring

---
+++ src/main/java/com/github/orionlibs/spring_http_request_logger/WebMvcConfiguration.java
@@ -0,0 +1,26 @@
+package com.github.orionlibs.spring_http_request_logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer
+{
+    private final LoggingInterceptor loggingInterceptor;
+
+
+    @Autowired
+    public WebMvcConfiguration(LoggingInterceptor loggingInterceptor)
+    {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry)
+    {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
